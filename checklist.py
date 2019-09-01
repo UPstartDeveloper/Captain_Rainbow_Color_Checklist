ce15a778ef3ba1c6cecd5cda2eb2d588ee4048ac
= <zainr7989@gmail.com>
2019-09-01 16:58:04 -0700

    Finish page 2 of tutorial.

--- checklist.py
+++ checklist.py
@@ -1,2 +1,38 @@
-print("Hello World")
+checklist = list()
 
+
+# CREATE
+def create(item):
+    checklist.append(item)
+
+
+# READ
+def read(index):
+    print(checklist[index])
+
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+
+# DESTROY
+def destroy(index):
+    checklist.pop(index)
+
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    print(read(1))
+
+
+test()
