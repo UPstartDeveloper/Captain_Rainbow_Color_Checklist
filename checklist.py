311afeaf27f9c2df478d1462d922fed1560455a6
= <zainr7989@gmail.com>
2019-09-01 19:36:18 -0700

    Complete stretch challenges.

--- checklist.py
+++ checklist.py
@@ -1,3 +1,4 @@
+import os
 checklist = list()
 
 
@@ -15,12 +16,12 @@ def read(index):
 
 # UPDATE
 def update(index, item):
-    checklist[index] = item
+    checklist[int(index)] = item
 
 
 # DESTROY
 def destroy(index):
-    checklist.pop(index)
+    checklist.pop(int(index))
 
 
 # LIST ALL ITEMS
@@ -33,7 +34,12 @@ def list_all_items():
 
 # MARKS ITEMS COMPLETE
 def mark_completed(index):
-    update(index, "{} {}".format("√", checklist[index]))
+    update(index, "{}{}".format("√", checklist[int(index)]))
+
+
+# UNCHECKS ITEMS
+def uncheck_item(index):
+    update(int(index), read(index)[1:])
 
 
 # SELECT
@@ -53,6 +59,19 @@ def select(function_code):
     # Print all items
     elif function_code == "P":
         list_all_items()
+
+    # Update an item in the list
+    elif function_code == "U":
+        item_index = input("Enter the index of the item to be updated: ")
+        item_update = input("Please enter what the item should say instead: ")
+        update(item_index, item_update)
+        print("\033[1;32;40m{}\x1b[0m".format("Item has been updated."))
+
+    # Delete an item from the list
+    elif function_code == "D":
+        item_index = input("Enter index number of the item to be deleted: ")
+        destroy(item_index)
+        print("\033[1;31;40m{}\x1b[0m".format("Item has been deleted."))
 
     elif function_code == "Q":
         return False
@@ -95,11 +114,25 @@ def test():
     print(user_value)
 
 
+'''
 # Run tests
 test()
-
+# Run tests for mark_completed and uncheck_item
+num = input("Enter a number to check: ")
+mark_completed(num)
+list_all_items()
+num = input("Enter the same number to uncheck: ")
+uncheck_item(num)
+list_all_items()
+'''
 running = True
 while running:
+    os.system('clear')  # clears terminal
     selection = user_input(
-        "Press C to add to list, R to Read from list, P to display list, and Q to quit ")
+        "Press C to add to list, \n" +
+        "R to Read from list, \n" +
+        "P to display list, \n" +
+        "U to update an item on the list \n" +
+        "D to delete an item from the list \n" +
+        "and Q to quit: ")
     running = select(selection)
